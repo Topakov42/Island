681ace93bf6d239cfca705c5f82798373b0734ae
Topakov <topa990048@mail.ru>
2026-04-08 17:36:50 +0700

    add Location class

--- src/main/java/com/javarush/model/Location.java
+++ src/main/java/com/javarush/model/Location.java
@@ -1,4 +1,45 @@
 package com.javarush.model;
 
+import com.javarush.animal.Animal;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Класс Локация содержит списки животных и объектов.
+ * todo записать прогу для многопоточки!!!
+ * Метод removePlant удаляет последнее растение без синххронизации (смотреть туду)
+ */
 public class Location {
+
+
+    @Getter
+    private final List<Animal> animals = new ArrayList<>();
+
+    @Getter
+    private final List<Plant> plants = new ArrayList<>();
+
+    public void addAnimal (Animal animal) {
+        animals.add(animal);
+    }
+
+    public void removeAnimal (Animal animal) {
+        animals.remove(animal);
+    }
+
+    public void addPlant (Plant plant ) {
+        plants.add (plant);
+    }
+
+
+    // Ментор сказал - для однопоточной версии. Как то надо будет развить по для многопоточки
+    public Plant removePlant () {
+        if (!plants.isEmpty()) {
+            return plants.remove(plants.size() -1 );
+        }
+        return null;
+    }
+
 }
