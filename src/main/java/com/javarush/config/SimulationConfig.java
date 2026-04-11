966ec2feb6438e5f96c13721e0c6f13c00c20ee0
Topakov <topa990048@mail.ru>
2026-04-11 16:40:08 +0700

    feat : add SimulationConfig with builder

---
+++ src/main/java/com/javarush/config/SimulationConfig.java
@@ -0,0 +1,28 @@
+package com.javarush.config;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Конфигурация приложения
+ */
+
+
+@Data
+@Builder
+public class SimulationConfig {
+    //размеры острова
+    private int islandWidth;
+    private int islandHeight;
+    // Популяции
+    private int initialWolf;
+    private int initialRabbit;
+    private int initialDeer;
+    private int plantsPerCell; // кол-во растение которые добавляются за 1 такт в каждую клетку.
+    // Дюрация в мс.
+    private long ticketDurationMs;
+    // Веротяности
+
+
+
+}
