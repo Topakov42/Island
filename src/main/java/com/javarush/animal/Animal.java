4a53f0617ef245f1d836393e480edbfec8fba938
Topakov <topa990048@mail.ru>
2026-04-18 11:39:37 +0700

    feat: add currentLocation and move method

--- src/main/java/com/javarush/animal/Animal.java
+++ src/main/java/com/javarush/animal/Animal.java
@@ -2,6 +2,8 @@ package com.javarush.animal;
 
 
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
 import com.javarush.model.Location;
 import com.javarush.model.Island;
 import lombok.Data;
@@ -18,13 +20,14 @@ public abstract class Animal {
     protected double maxSatiety; // сытость максимальная
     protected double currentSatiety; // текущая сытость
     protected boolean alive = true;  // статус жизни животного
-    protected  double speed = 1; // скорость перемещения
+    protected double speed = 1; // скорость перемещения
     protected double maxCountPerCell; // максмальное количество животных этого ввида
+    protected volatile Location currentLocation; // текущее местонахождение животного
 
 
     // Карта вероятности поедания других животных
 
-    protected Map < Class  <? extends Animal> , Integer> eatingProbabilities;
+    protected Map<Class<? extends Animal>, Integer> eatingProbabilities;
 
     public Animal(double weight, double maxSatiety) {
         this.weight = weight;
@@ -35,16 +38,36 @@ public abstract class Animal {
     // eat , move , reproduce
 
 
-    public abstract void eat (Location location);
+    public abstract void eat(Location location);
 
-    public abstract void move(Island island, int currentX, int currentY);
+    public void move(Island island, int currentX, int currentY) {
+        if (!alive) {
+            return;
+        }
+        if (currentLocation == null) {
+            log.warn("Животное {} не имеет текущей локации. Перемещение не возможно", this);
+            return;
+        }
+        int dirction = ThreadLocalRandom.current().nextInt(4);
+        int newX = currentX;
+        int newY = currentY;
+
+        switch (dirction) {
+            case 0: newY = Math.max(0, currentY - 1); break;  // вверх Y
+            case 1: newX = Math.min(island.getWidth() - 1, currentX + 1 ); break; // вправо X
+            case 2: newY = Math.min(island.getHeight() - 1, currentY + 1 ); break; // вниз Y
+            case 3: newX = Math.max(0, currentX - 1); break; // влево X
+
+        }
+    }
+
+    ;
 
     public abstract void reproduce(Location location);
 
-    public void die () {
+    public void die() {
         this.alive = false;
     }
-
 
 
 }
