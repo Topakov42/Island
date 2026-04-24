80030031b13247c0fa800e7d2a4a561c980b0e02
Topakov <topa990048@mail.ru>
2026-04-24 13:08:06 +0700

    work version

--- src/main/java/com/javarush/App.java
+++ src/main/java/com/javarush/App.java
@@ -14,6 +14,7 @@ public class App {
         multithreadSimulation.initialize();
         multithreadSimulation.start();
 
+
         log.info(" Начальное состояние : ");
         multithreadSimulation.printStatistics();
 
